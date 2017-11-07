e615d03992b84d5310329a719d32bd76beb5f14d
barbolute <33287182+barbolute@users.noreply.github.com>
2017-11-06 23:47:28 -0500

    Update aligndata.sh

--- aligndata.sh
+++ aligndata.sh
@@ -14,55 +14,55 @@ IDXLRR=lrr/LRR.fa
 
 R1=reads/SRR5001851_1.fastq.gz
 R2=reads/SRR5001851_2.fastq.gz
-BAM=bam/C1_SRR.bam
+BAM=bam/SRR_C1.bam
 hisat2 -p 16 -x $IDXSRR -1 $R1 -2 $R2 2>> $RUNLOG | samtools sort -l 1.6G -@16 > $BAM 2>> $RUNLOG
 samtools index -@16 $BAM
-BAM=bam/C1_LRR.bam
+BAM=bam/LRR_C1.bam
 hisat2 -p 16 -x $IDXLRR -1 $R1 -2 $R2 2>> $RUNLOG | samtools sort -l 1.6G -@16 > $BAM 2>> $RUNLOG
 samtools index -@16 $BAM
 
 R1=reads/SRR5001848_1.fastq.gz
 R2=reads/SRR5001848_2.fastq.gz
-BAM=bam/C2_SRR.bam
+BAM=bam/SRR_C2.bam
 hisat2 -p 16 -x $IDXSRR -1 $R1 -2 $R2 2>> $RUNLOG | samtools sort -l 1.6G -@16 > $BAM 2>> $RUNLOG
 samtools index -@16 $BAM
-BAM=bam/C2_LRR.bam
+BAM=bam/LRR_C2.bam
 hisat2 -p 16 -x $IDXLRR -1 $R1 -2 $R2 2>> $RUNLOG | samtools sort -l 1.6G -@16 > $BAM 2>> $RUNLOG
 samtools index -@16 $BAM
 
 R1=reads/SRR5001850_1.fastq.gz
 R2=reads/SRR5001850_2.fastq.gz
-BAM=bam/C3_SRR.bam
+BAM=bam/SRR_C3.bam
 hisat2 -p 16 -x $IDXSRR -1 $R1 -2 $R2 2>> $RUNLOG | samtools sort -l 1.6G -@16 > $BAM 2>> $RUNLOG
 samtools index -@16 $BAM
-BAM=bam/C3_LRR.bam
+BAM=bam/LRR_C3.bam
 hisat2 -p 16 -x $IDXLRR -1 $R1 -2 $R2 2>> $RUNLOG | samtools sort -l 1.6G -@16 > $BAM 2>> $RUNLOG
 samtools index -@16 $BAM
 
 R1=reads/SRR5001844_1.fastq.gz
 R2=reads/SRR5001844_2.fastq.gz
-BAM=bam/T1_SRR.bam
+BAM=bam/SRR_T1.bam
 hisat2 -p 16 -x $IDXSRR -1 $R1 -2 $R2 2>> $RUNLOG | samtools sort -l 1.6G -@16 > $BAM 2>> $RUNLOG
 samtools index -@16 $BAM
-BAM=bam/T1_LRR.bam
+BAM=bam/LRR_T1.bam
 hisat2 -p 16 -x $IDXLRR -1 $R1 -2 $R2 2>> $RUNLOG | samtools sort -l 1.6G -@16 > $BAM 2>> $RUNLOG
 samtools index -@16 $BAM
 
 R1=reads/SRR5001847_1.fastq.gz
 R2=reads/SRR5001847_2.fastq.gz
-BAM=bam/T2_SRR.bam
+BAM=bam/SRR_T2.bam
 hisat2 -p 16 -x $IDXSRR -1 $R1 -2 $R2 2>> $RUNLOG | samtools sort -l 1.6G -@16 > $BAM 2>> $RUNLOG
 samtools index -@16 $BAM
-BAM=bam/T2_LRR.bam
+BAM=bam/LRR_T2.bam
 hisat2 -p 16 -x $IDXLRR -1 $R1 -2 $R2 2>> $RUNLOG | samtools sort -l 1.6G -@16 > $BAM 2>> $RUNLOG
 samtools index -@16 $BAM
 
 R1=reads/SRR5001846_1.fastq.gz
 R2=reads/SRR5001846_2.fastq.gz
-BAM=bam/T3_SRR.bam
+BAM=bam/SRR_T3.bam
 hisat2 -p 16 -x $IDXSRR -1 $R1 -2 $R2 2>> $RUNLOG | samtools sort -l 1.6G -@16 > $BAM 2>> $RUNLOG
 samtools index -@16 $BAM
-BAM=bam/T3_LRR.bam
+BAM=bam/LRR_T3.bam
 hisat2 -p 16 -x $IDXLRR -1 $R1 -2 $R2 2>> $RUNLOG | samtools sort -l 1.6G -@16 > $BAM 2>> $RUNLOG
 samtools index -@16 $BAM
 
