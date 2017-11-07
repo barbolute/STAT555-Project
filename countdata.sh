f5818b034bc6ba76ac13e4658fecf9549cf1ddce
barbolute <33287182+barbolute@users.noreply.github.com>
2017-11-07 00:31:42 -0500

    Update countdata.sh

--- countdata.sh
+++ countdata.sh
@@ -1,1 +1,27 @@
+# Stop this script on any error.
+set -euo pipefail
 
+# The gene feature file
+GTFSRR=srr/SRR.gff
+GTFLRR=lrr/LRR.gff
+
+# Collect the output of commands here.
+RUNLOG=runlog.txt
+
+echo "*** Counting features with: $GTFSRR"
+featureCounts -F GTF -a $GTFSRR -g gene_name -o counts.txt  bam/SRR_C*.bam  bam/SRR_T*.bam 2>> $RUNLOG
+
+echo "*** Generating simple counts."
+cat counts.txt | cut -f 1,7-12 > simple_counts.txt
+
+echo "*** Running DESeq1."
+curl -s -O http://data.biostarhandbook.com/rnaseq/code/deseq1.r
+cat simple_counts.txt | Rscript deseq1.r 3x3 > results_deseq1.txt  2>> $RUNLOG
+
+echo "*** Running DESeq2."
+curl -s -O http://data.biostarhandbook.com/rnaseq/code/deseq2.r
+cat simple_counts.txt | Rscript deseq2.r 3x3 > results_deseq2.txt  2>> $RUNLOG
+
+echo "*** Running EdgeR."
+curl -s -O http://data.biostarhandbook.com/rnaseq/code/edger.r
+cat simple_counts.txt | Rscript edger.r 3x3 > results_edger.txt  2>> $RUNLOG
