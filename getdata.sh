5c905599a61b2ebb8261b3957bc95be777606acc
barbolute <33287182+barbolute@users.noreply.github.com>
2017-11-17 03:48:49 -0500

    Update getdata.sh

--- getdata.sh
+++ getdata.sh
@@ -23,26 +23,24 @@ GFFLRR=lrr/LRR.gff*
 
 # Download and unpack the raw RNA-Seq Data for the DE part of experiment
 #from spleen
-fastq-dump --gzip --split-files -X 10000 -O reads SRR5001851
-fastq-dump --gzip --split-files -X 10000 -O reads SRR5001848
-fastq-dump --gzip --split-files -X 10000 -O reads SRR5001850
-fastq-dump --gzip --split-files -X 10000 -O reads SRR5001844
-fastq-dump --gzip --split-files -X 10000 -O reads SRR5001847
-fastq-dump --gzip --split-files -X 10000 -O reads SRR5001846
+fastq-dump --gzip --split-files -X 10000 -O rawdata SRR5001851
+fastq-dump --gzip --split-files -X 10000 -O rawdata SRR5001848
+fastq-dump --gzip --split-files -X 10000 -O rawdata SRR5001850
+fastq-dump --gzip --split-files -X 10000 -O rawdata SRR5001844
+fastq-dump --gzip --split-files -X 10000 -O rawdata SRR5001847
+fastq-dump --gzip --split-files -X 10000 -O rawdata SRR5001846
 
 #from brain for mapping based transcriptome assembly
-fastq-dump --gzip --split-files -O assemblydata SRR1619467
+fastq-dump --gzip --split-files -O rawdata SRR1619467
 
 #from muscle tissue for mapping based transcriptome assembly
-fastq-dump --gzip --split-files -O assemblydata SRR2545947
+fastq-dump --gzip --split-files -O rawdata SRR2545947
 
 #from G226 Zebra Finch Cell Line for mapping based transcriptome assembly
-fastq-dump --gzip --split-files -O assemblydata SRR1564481
+fastq-dump --gzip --split-files -O rawdata SRR1564481
 
 #from ZFTNA Zebra Finch Cell Line for mapping based transcriptome assembly
-fastq-dump --gzip --split-files -O assemblydata SRR1564483
-
-ls
+fastq-dump --gzip --split-files -O rawdata SRR1564483
 
 # Download and unpack the short read reference genome and gff file.
 URL1=ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/151/805/GCA_000151805.2_Taeniopygia_guttata-3.2.4/GCA_000151805.2_Taeniopygia_guttata-3.2.4_genomic.fna.gz
@@ -66,3 +64,5 @@ gunzip $REFLRR
 echo "*** Building hisat indices: $IDXSRR and $IDXLRR"
 hisat2-build -p 14 $REFSRR $IDXSRR
 hisat2-build -p 14 $REFLRR $IDXLRR
+
+ls
