6b018f46f71c12995014f638b1ed187322d4064a
barbolute <33287182+barbolute@users.noreply.github.com>
2017-11-17 03:33:49 -0500

    Update countdata.sh

--- countdata.sh
+++ countdata.sh
@@ -11,7 +11,7 @@ RUNLOG=runlog.txt
 echo "*** Counting features with: $GTFSRR"
 featureCounts -T 14 -a $GTFSRR -g gene_name -o LRR_Cuffcounts.txt  bam/LRR_C*.bam  bam/LRR_T*.bam 2>> $RUNLOG
 
-
+featureCounts -T 16 -a merged.gtf -g gene_id -o mergedcounts.txt SRR/bam/SRR_*.bam LRR/bam/LRR_*.bam 
 
 
 #simple counts
