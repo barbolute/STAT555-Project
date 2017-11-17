aab50c60437bede80a574987674d450f483a8685
barbolute <33287182+barbolute@users.noreply.github.com>
2017-11-17 01:50:17 -0500

    Update getdata.sh

--- getdata.sh
+++ getdata.sh
@@ -4,7 +4,7 @@
 # Stop on any error.
 set -ueo pipefail
 
-# Create output foldes
+# Create output folders
 mkdir -p srr
 mkdir -p lrr
 
@@ -21,13 +21,26 @@ IDXLRR=lrr/LRR.fa
 GFFSRR=srr/SRR.gff*
 GFFLRR=lrr/LRR.gff*
 
-# Download and unpack the raw RNA-Seq Data
+# Download and unpack the raw RNA-Seq Data for the DE part of experiment
+#from spleen
 fastq-dump --gzip --split-files -X 10000 -O reads SRR5001851
 fastq-dump --gzip --split-files -X 10000 -O reads SRR5001848
 fastq-dump --gzip --split-files -X 10000 -O reads SRR5001850
 fastq-dump --gzip --split-files -X 10000 -O reads SRR5001844
 fastq-dump --gzip --split-files -X 10000 -O reads SRR5001847
 fastq-dump --gzip --split-files -X 10000 -O reads SRR5001846
+
+#from brain for mapping based transcriptome assembly
+fastq-dump --gzip --split-files -X 10000 -O assemblydata SRR1619467
+
+#from muscle tissue for mapping based transcriptome assembly
+fastq-dump --gzip --split-files -X 10000 -O assemblydata SRR2545947
+
+#from G226 Zebra Finch Cell Line for mapping based transcriptome assembly
+fastq-dump --gzip --split-files -X 10000 -O assemblydata SRR1564481
+
+#from ZFTNA Zebra Finch Cell Line for mapping based transcriptome assembly
+fastq-dump --gzip --split-files -X 10000 -O assemblydata SRR1564483
 
 # Download and unpack the short read reference genome and gff file.
 URL1=ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/151/805/GCA_000151805.2_Taeniopygia_guttata-3.2.4/GCA_000151805.2_Taeniopygia_guttata-3.2.4_genomic.fna.gz
@@ -40,17 +53,12 @@ echo "*** Downloading: $URL2"
 curl -s $URL2 > srr/SRR.gff.gz
 gunzip $GFFSRR
 
-# Download and unpack the Long Read Reference genome and gff file.
+# Download and unpack the Long Read Reference genome
 
 URL3=ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/002/008/985/GCA_002008985.2_Tgut_diploid_1.0/GCA_002008985.2_Tgut_diploid_1.0_genomic.fna.gz
 echo "*** Downloading: $URL3"
 curl -s $URL3 > lrr/LRR.fa.gz
 gunzip $REFLRR
-
-URL4=ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/002/008/985/GCA_002008985.2_Tgut_diploid_1.0/GCA_002008985.2_Tgut_diploid_1.0_genomic.gff.gz
-echo "*** Downloading: $URL4"
-curl $URL4 > lrr/LRR.gff.gz
-gunzip $GFFLRR
 
 # Build the indices
 echo "*** Building hisat indices: $IDXSRR and $IDXLRR"
