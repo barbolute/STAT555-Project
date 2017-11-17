c90ad1d2f0d7e09e7ce2a4ee3884ae289e3a75d4
barbolute <33287182+barbolute@users.noreply.github.com>
2017-11-17 01:44:26 -0500

    Update cufflinksassemblydata.sh

--- cufflinksassemblydata.sh
+++ cufflinksassemblydata.sh
@@ -11,16 +11,16 @@ fastq-dump --gzip --split-files -X 10000 -O reads SRR5001847
 fastq-dump --gzip --split-files -X 10000 -O reads SRR5001846
 
 #from brain 
-fastq-dump --gzip --split-files -O assemblydata SRR1619467
+fastq-dump --gzip --split-files -X 10000 -O assemblydata SRR1619467
 
 #from muscle tissue
-fastq-dump --gzip --split-files -O assemblydata SRR2545947
+fastq-dump --gzip --split-files -X 10000 -O assemblydata SRR2545947
 
 #from G226 Zebra Finch Cell Line
-fastq-dump --gzip --split-files -O assemblydata SRR1564481
+fastq-dump --gzip --split-files -X 10000 -O assemblydata SRR1564481
 
 #from ZFTNA Zebra Finch Cell Line
-fastq-dump --gzip --split-files -O assemblydata SRR1564483
+fastq-dump --gzip --split-files -X 10000 -O assemblydata SRR1564483
 
 
 
